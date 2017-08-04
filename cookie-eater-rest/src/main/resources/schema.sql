89a113d8f3717fc727a6a8d47cdb0c31a96c6c5b
Milica Zivkov <milica.zivkov@codecentric.de>
2017-08-04 10:55:59 +0200

    remove duplicated cookie DDL

--- cookie-eater-rest/src/main/resources/schema.sql
+++ cookie-eater-rest/src/main/resources/schema.sql
@@ -1,11 +1,8 @@
+DROP TABLE IF EXISTS sweets.cookie;
+
 CREATE TABLE IF NOT EXISTS sweets.cookie (
    id INT AUTO_INCREMENT PRIMARY KEY,
    name VARCHAR(50) NOT NULL,
    username VARCHAR(50) NOT NULL,
    CONSTRAINT fk_cookie_user FOREIGN KEY (username) REFERENCES sweets.users (username) ON DELETE NO ACTION
-);
-
-CREATE TABLE IF NOT EXISTS sweets.cookie (
-   id INT AUTO_INCREMENT PRIMARY KEY,
-   name VARCHAR(50) NOT NULL
 );
